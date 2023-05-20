e786665a3fc4668ffe72accfc402643a806bb6ce
valentinesamuel <valentinesamuel2580@gmail.com>
2023-05-20 13:45:52 +0100

    FEAT: Made inventory rsponsive

--- src/routes/Manager/ManagerInventory/ManagerInventory.component.tsx
+++ src/routes/Manager/ManagerInventory/ManagerInventory.component.tsx
@@ -50,16 +50,21 @@ const ManagerInventory = () => {
         width: '100%',
         // height: '100vh',
         overflowX: 'auto',
-        padding: '1% 3% 3% 3%',
+        padding: { desktop: '1% 3% 3% 3%', mobile: '1% 20px 20px 20px' },
       }}
     >
       <InventoryMetrics />
-
       <Box
         sx={{
           display: 'flex',
-          alignItems: 'center',
+          alignItems: { desktop: 'center', tablet: 'stretch' },
           justifyContent: 'space-between',
+
+          flexDirection: {
+            mobile: 'column',
+            desktop: 'row',
+          },
+          margin: { mobile: '6% 0 6% 0', desktop: '1% 0 0% 0' },
         }}
       >
         <Box
@@ -68,14 +73,16 @@ const ManagerInventory = () => {
             borderColor: 'divider',
             display: 'flex',
             justifyContent: 'space-between',
-            margin: '2% 0 2% 0',
-            width: 'fit-content',
+            margin: { mobile: '0 0 6% 0', desktop: '1% 0 1% 0' },
+            width: { desktop: 'fit-content', mobile: '100%' },
           }}
         >
           <Tabs
             value={currentTab}
             onChange={handleTabChange}
-            aria-label="basic tabs example"
+            variant="scrollable"
+            scrollButtons="auto"
+            aria-label="scrollable auto tabs example"
           >
             <Tab label="All Products" {...a11yProps(0)} />
             <Tab label="Low in stock" {...a11yProps(1)} />
@@ -83,13 +90,15 @@ const ManagerInventory = () => {
             <Tab label="Expired Products" {...a11yProps(3)} />
           </Tabs>
         </Box>
-        <Box sx={{ display: 'flex' }}>
+        <Box sx={{
+          display: 'flex', justifyContent: {
+            tablet: 'space-between'
+        } }}>
           <SelectCategories />
           <Box
             sx={{
               //   p: '2px 4px',
               display: 'flex',
-              alignItems: 'center',
             }}
           >
             <OutlinedInput
